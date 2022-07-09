96c2980e56a1d6cf5d6d1d938c46a0afe6ee70ca
Monika Gustilo <monikagustilo@gmail.com>
2022-07-09 16:18:44 -0700

    commit tree ded193bc6329ae15429cf299975a55d95cdc9a8d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+function superbowlWin(arr) {
+  let results = arr.find((e) => e.result === "W");
+  if (results) {
+    return results.year;
+  }
+}
+
+console.log(superbowlWin(record));
